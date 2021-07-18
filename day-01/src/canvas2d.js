c097957036a9cbee1d6004b6f806e24afdff6478
Jan Kohlbach <mail@jankohlbach.dev>
2021-07-18 17:06:35 +0200

    day 1

---
+++ day-01/src/canvas2d.js
@@ -0,0 +1,36 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+// ctx.fillRect(0, 0, 100, 50);
+
+function calculatePixelIndices(top, left, width, height) {
+  const pixelIndices = [];
+
+  for (let x = left; x < left + width; x++) {
+    for (let y = top; y < top + height; y++) {
+      const i = y * canvas.width * 4 + x * 4;
+
+      pixelIndices.push(i);
+    }
+  }
+
+  return pixelIndices;
+}
+
+function fillRect(top, left, width, height) {
+  const pixelStore = new Uint8ClampedArray(canvas.width * canvas.height * 4);
+
+  const pixelIndices = calculatePixelIndices(top, left, width, height);
+
+  pixelIndices.forEach((i) => {
+    pixelStore[i] = 0;  // r
+    pixelStore[i + 1] = 0;  // g
+    pixelStore[i + 2] = 0;  // b
+    pixelStore[i + 3] = 255;  // alpha
+  });
+
+  const imageData = new ImageData(pixelStore, canvas.width, canvas.height);
+  ctx.putImageData(imageData, 0, 0);
+}
+
+fillRect(10, 10, 100, 50);
