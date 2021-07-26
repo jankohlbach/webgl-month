c77950155f6e6b29d8a99ce208abf70690a95964
Jan Kohlbach <mail@jankohlbach.dev>
2021-07-26 12:34:06 +0200

    day 3 - triangles

--- day-03/src/webgl-hello-world.js
+++ day-03/src/webgl-hello-world.js
@@ -20,8 +20,11 @@ const vShaderSrc = `
 `;
 
 const fShaderSrc = `
+  precision mediump float;
+  uniform vec4 color;
+
   void main() {
-    gl_FragColor = vec4(0, 0, 0, 1);
+    gl_FragColor = color / 255.0;
   }
 `;
 
@@ -48,21 +51,18 @@ gl.useProgram(program);
 
 const positionPointer = gl.getAttribLocation(program, 'position');
 const resolutionUniformLocation = gl.getUniformLocation(program, 'resolution');
+const colorUniformLocation = gl.getUniformLocation(program, 'color');
 
 gl.uniform2fv(resolutionUniformLocation, [canvas.width, canvas.height]);
+gl.uniform4fv(colorUniformLocation, [255, 0, 0, 255]);
 
-const lines = [];
-let prevLineY = 0;
+const triangles = [
+  0, 0,
+  canvas.width / 2, canvas.height,
+  canvas.width, 0,
+];
 
-for (let i = 0; i < canvas.width - 5; i+=5) {
-  lines.push(i, prevLineY);
-  const y = Math.random() * canvas.height;
-  lines.push(i + 5, y);
-
-  prevLineY = y;
-}
-
-const positionData = new Float32Array(lines);
+const positionData = new Float32Array(triangles);
 
 const positionBuffer = gl.createBuffer(gl.ARRAY_BUFFER);
 
@@ -78,4 +78,4 @@ const offset = 0;
 gl.enableVertexAttribArray(positionPointer);
 gl.vertexAttribPointer(positionPointer, attributeSize, type, normalized, stride, offset);
 
-gl.drawArrays(gl.LINES, 0, positionData.length / 2);
+gl.drawArrays(gl.TRIANGLES, 0, positionData.length / 2);
