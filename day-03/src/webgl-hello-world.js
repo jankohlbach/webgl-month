c283d63694040b7020022243de440cc57cdd6a61
Jan Kohlbach <mail@jankohlbach.dev>
2021-07-26 12:33:26 +0200

    day 3

---
+++ day-03/src/webgl-hello-world.js
@@ -0,0 +1,81 @@
+const canvas = document.querySelector('canvas');
+const gl = canvas.getContext('webgl');
+
+const program = gl.createProgram();
+
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+const vShaderSrc = `
+  attribute vec2 position;
+  uniform vec2 resolution;
+
+  #define M_PI 3.1415926535897932384626433832795
+
+  void main() {
+    vec2 transformedPosition = position / resolution * 2.0 - 1.0;
+    gl_PointSize = 2.0;
+    gl_Position = vec4(transformedPosition, 0, 1);
+  }
+`;
+
+const fShaderSrc = `
+  void main() {
+    gl_FragColor = vec4(0, 0, 0, 1);
+  }
+`;
+
+function compileShader(shader, source) {
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  const log = gl.getShaderInfoLog(shader);
+
+  if (log) {
+    throw new Error(log);
+  }
+}
+
+compileShader(vertexShader, vShaderSrc);
+compileShader(fragmentShader, fShaderSrc);
+
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+
+gl.linkProgram(program);
+
+gl.useProgram(program);
+
+const positionPointer = gl.getAttribLocation(program, 'position');
+const resolutionUniformLocation = gl.getUniformLocation(program, 'resolution');
+
+gl.uniform2fv(resolutionUniformLocation, [canvas.width, canvas.height]);
+
+const lines = [];
+let prevLineY = 0;
+
+for (let i = 0; i < canvas.width - 5; i+=5) {
+  lines.push(i, prevLineY);
+  const y = Math.random() * canvas.height;
+  lines.push(i + 5, y);
+
+  prevLineY = y;
+}
+
+const positionData = new Float32Array(lines);
+
+const positionBuffer = gl.createBuffer(gl.ARRAY_BUFFER);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, positionData, gl.STATIC_DRAW);
+
+const attributeSize = 2;
+const type = gl.FLOAT;
+const normalized = false;
+const stride = 0;
+const offset = 0;
+
+gl.enableVertexAttribArray(positionPointer);
+gl.vertexAttribPointer(positionPointer, attributeSize, type, normalized, stride, offset);
+
+gl.drawArrays(gl.LINES, 0, positionData.length / 2);
