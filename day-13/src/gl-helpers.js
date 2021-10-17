1095e72c304364fa2cdbccdee95279b1de54bac2
Jan Kohlbach <mail@jankohlbach.dev>
2021-10-17 20:43:00 +0200

    day 13

---
+++ day-13/src/gl-helpers.js
@@ -0,0 +1,83 @@
+import extract from 'glsl-extract-sync';
+
+export function compileShader(gl, shader, source) {
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  const log = gl.getShaderInfoLog(shader);
+
+  if (log) {
+    throw new Error(log);
+  }
+}
+
+export function setupShaderInput(gl, program, vShaderSource, fShaderSource) {
+  const vShaderInfo = extract(vShaderSource);
+  const fShaderInfo = extract(fShaderSource);
+
+  const attributes = vShaderInfo.attributes;
+  const uniforms = [
+    ...vShaderInfo.uniforms,
+    ...fShaderInfo.uniforms,
+  ];
+
+  const attributeLocations = attributes.reduce((attrsMap, attr) => {
+    attrsMap[attr.name] = gl.getAttribLocation(program, attr.name);
+    return attrsMap;
+  }, {});
+
+  attributes.forEach((attr) => {
+    gl.enableVertexAttribArray(attributeLocations[attr.name]);
+  });
+
+  const uniformLocations = uniforms.reduce((uniformsMap, uniform) => {
+    uniformsMap[uniform.name] = gl.getUniformLocation(program, uniform.name);
+    return uniformsMap;
+  }, {});
+
+  return {
+    attributeLocations,
+    uniformLocations,
+  };
+}
+
+export async function loadImage(src) {
+  const img = new Image();
+
+  let _resolve;
+  const p = new Promise((resolve) => _resolve = resolve);
+
+  img.onload = () => {
+    _resolve(img);
+  }
+
+  img.src = src;
+
+  return p;
+}
+
+export function createTexture(gl) {
+  const texture = gl.createTexture();
+
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+  return texture;
+}
+
+export function setImage(gl, texture, img) {
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    img,
+  );
+}
