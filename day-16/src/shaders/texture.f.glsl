1273af698c546869ad03e74f85502b7444975252
Jan Kohlbach <mail@jankohlbach.dev>
2021-10-31 21:22:19 +0100

    day 16

---
+++ day-16/src/shaders/texture.f.glsl
@@ -0,0 +1,32 @@
+precision mediump float;
+
+uniform sampler2D texture;
+uniform sampler2D otherTexture;
+uniform vec2 resolution;
+
+vec4 inverse(vec4 color) {
+  return abs(vec4(color.rgb - 1.0, color.a));
+}
+
+vec4 blackAndWhite(vec4 color) {
+  return vec4(vec3(1.0, 1.0, 1.0) * (color.r + color.g + color.b) / 3.0, color.a);
+}
+
+vec4 sepia(vec4 color) {
+  vec3 sepiaColor = vec3(112, 66, 20) / 255.0;
+
+  return vec4(mix(color.rgb, sepiaColor, 0.4), color.a);
+}
+
+varying vec2 vTexCoord;
+varying float vTexIndex;
+
+void main() {
+  vec2 texCoord = vTexCoord;
+
+  if (vTexIndex == 0.0) {
+    gl_FragColor = texture2D(texture, texCoord);
+  } else {
+    gl_FragColor = texture2D(otherTexture, texCoord);
+  }
+}
---
+++ day-16/src/shape-helpers.js
@@ -0,0 +1,50 @@
+const PI_4 = Math.PI / 4;
+
+export function createRect(top, left, width, height, angle = 0) {
+  const centerX = width / 2;
+  const centerY = height / 2;
+
+  const diagonalLength = Math.sqrt(centerX ** 2 + centerY ** 2);
+
+  const x1 = centerX + diagonalLength * Math.cos(angle + PI_4);
+  const y1 = centerY + diagonalLength * Math.sin(angle + PI_4);
+
+  const x2 = centerX + diagonalLength * Math.cos(angle + PI_4 * 3);
+  const y2 = centerY + diagonalLength * Math.sin(angle + PI_4 * 3);
+
+  const x3 = centerX + diagonalLength * Math.cos(angle - PI_4);
+  const y3 = centerY + diagonalLength * Math.sin(angle - PI_4);
+
+  const x4 = centerX + diagonalLength * Math.cos(angle - PI_4 * 3);
+  const y4 = centerY + diagonalLength * Math.sin(angle - PI_4 * 3);
+
+  return [
+    x1 + left, y1 + top,
+    x2 + left, y2 + top,
+    x3 + left, y3 + top,
+    x4 + left, y4 + top,
+  ];
+}
+
+export function createHexagon(centerX, centerY, radius, segmentsCount) {
+  const vertexData = [];
+  const segmentAngle =  Math.PI * 2 / segmentsCount;
+
+  for (let i = 0; i < Math.PI * 2; i += segmentAngle) {
+    const from = i;
+    const to = i + segmentAngle;
+
+    const color = rainbowColors[i / segmentAngle];
+
+    vertexData.push(centerX, centerY);
+    vertexData.push(...color);
+
+    vertexData.push(centerX + Math.cos(from) * radius, centerY + Math.sin(from) * radius);
+    vertexData.push(...color);
+
+    vertexData.push(centerX + Math.cos(to) * radius, centerY + Math.sin(to) * radius);
+    vertexData.push(...color);
+  }
+
+  return vertexData;
+}
