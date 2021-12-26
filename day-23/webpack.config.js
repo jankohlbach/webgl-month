e3e0ba946e3f14f1457850a147e7bfe21c210263
Jan Kohlbach <mail@jankohlbach.dev>
2021-12-26 14:22:43 +0100

    day 23

---
+++ day-23/webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    'skybox': './src/skybox.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(glsl|obj)$/,
+        use: 'raw-loader',
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: 'url-loader',
+      },
+    ],
+  },
+  resolve: {
+    fallback: {
+      'stream': require.resolve('stream-browserify'),
+      'buffer': require.resolve('buffer/'),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
+  mode: 'development',
+};
